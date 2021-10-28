6c3660359ed12694ed65bc491d1871ccb023bdfc
Will Berner <will.o.berner@gmail.com>
2021-10-28 12:24:57 -0400

    Started working on server.

--- server/server.js
+++ server/server.js
@@ -1,21 +1,41 @@
+// Import dependencies and database config
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
 
+// Import Apollo Server
+const { ApolloServer } = require('apollo-server-express');
+
+// Import middleware for authorization
+const { authMiddleware } = require('./utils/auth');
+
+// Import GraphQL typeDefs and Resolvers
+const { typeDefs, resolvers } = require('./schemas');
+
+// Set up express app
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Set up Apollo Server using auth middleware
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+
+// Boilerplate
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
+// If in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// Hook up Apollo Server to use Express
+server.applyMiddleware({ app });
 
+// Once database connection has been established, start the server
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
