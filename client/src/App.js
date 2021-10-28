3085ca7ba6b7a65c8ae7c528c7bc6e07bf864a4b
Will Berner <will.o.berner@gmail.com>
2021-10-28 13:46:26 -0400

    Added Apollo client to frontend app.

--- client/src/App.js
+++ client/src/App.js
@@ -4,18 +4,45 @@ import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+// Import Apollo client dependencies
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+
+// Set up the Apollo Client
+const apolloClient = new ApolloClient({
+
+  // For each request, send user's JWT token
+  request: operation => {
+
+    // Attempt to get the token of the user to authenticate them
+    const token = localStorage.getItem('id_token');
+
+    // Set the context of the user with their retreived JWT for backend
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    });
+  },
+
+  uri: '/graphql'
+});
+
+
 function App() {
   return (
-    <Router>
-      <>
-        <Navbar />
-        <Switch>
-          <Route exact path='/' component={SearchBooks} />
-          <Route exact path='/saved' component={SavedBooks} />
-          <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
-        </Switch>
-      </>
-    </Router>
+    <ApolloProvider client={apolloClient}>
+      <Router>
+        <>
+          <Navbar />
+          <Switch>
+            <Route exact path='/' component={SearchBooks} />
+            <Route exact path='/saved' component={SavedBooks} />
+            <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
+          </Switch>
+        </>
+      </Router>
+    </ApolloProvider>
   );
 }
 
